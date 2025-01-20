fe933781f53f90b18c0efc1d171c1c6a7a45f327
rn <rn@gmail.com>
2025-01-20 11:58:05 +0700

    ui:sidebar

--- obj/Release/net8.0/Barang.AssemblyInfo.cs
+++ obj/Release/net8.0/Barang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e35c9ac044d022d1ef4f04ee5dd58db8642f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
