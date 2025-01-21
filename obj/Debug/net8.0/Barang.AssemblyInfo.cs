3a042f0769c392f3433d4c7039a06a2fcc19ef22
rn <rn@gmail.com>
2025-01-21 14:19:29 +0700

    fixing layout and responsive

--- obj/Debug/net8.0/Barang.AssemblyInfo.cs
+++ obj/Debug/net8.0/Barang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe933781f53f90b18c0efc1d171c1c6a7a45f327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+375a6705258872013fe99e76eb35b432786bbd05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
