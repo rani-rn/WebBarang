375a6705258872013fe99e76eb35b432786bbd05
rn <rn@gmail.com>
2025-01-21 10:29:36 +0700

    fixing layout: not responsive

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,35 +1,33 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebarToggle = document.querySelector("#sidebar-toggle");
+    sidebarToggle.addEventListener("click", function () {
+        document.querySelector("#sidebar").classList.toggle("collapsed");
+    });
 
-// Write your JavaScript code.
-const sidebarToggle = document.querySelector("#sidebar-toggle");
-sidebarToggle.addEventListener("click",function(){
-    document.querySelector("#sidebar").classList.toggle("collapsed");
-});
+    document.querySelector(".theme-toggle").addEventListener("click", () => {
+        toggleLocalStorage();
+        toggleRootClass();
+    });
 
-document.querySelector(".theme-toggle").addEventListener("click",() => {
-    toggleLocalStorage();
-    toggleRootClass();
-});
-
-function toggleRootClass(){
-    const current = document.documentElement.getAttribute('data-bs-theme');
-    const inverted = current == 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-bs-theme',inverted);
-}
-
-function toggleLocalStorage(){
-    if(isLight()){
-        localStorage.removeItem("light");
-    }else{
-        localStorage.setItem("light","set");
+    function toggleRootClass() {
+        const current = document.documentElement.getAttribute('data-bs-theme');
+        const inverted = current == 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute('data-bs-theme', inverted);
     }
-}
 
-function isLight(){
-    return localStorage.getItem("light");
-}
+    function toggleLocalStorage() {
+        if (isLight()) {
+            localStorage.removeItem("light");
+        } else {
+            localStorage.setItem("light", "set");
+        }
+    }
 
-if(isLight()){
-    toggleRootClass();
-}
+    function isLight() {
+        return localStorage.getItem("light");
+    }
+
+    if (isLight()) {
+        toggleRootClass();
+    }
+});
